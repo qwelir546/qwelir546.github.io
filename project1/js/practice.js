fbcdbf208479505a82a60ff6696d41fd7eb7df69
qwelir546 <109228110+qwelir546@users.noreply.github.com>
2022-09-22 04:50:00 +0700

    Add files via upload
    проблема с анимацией

--- project1/js/practice.js
+++ project1/js/practice.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
     
+    var exam = 0;
+    
     $(".preloader").css("display", "none"); 
     
     
@@ -19,8 +21,109 @@ event.preventDefault();
         return false;
     });
     
+    
+    const time = 1500;
+    const step = 1;
+    const step1 = 25;
+    const step3 = 1;
+    var flag = 0;
+    
+    
+    if (flag == 0)
+        {
+
+            
+            
+            
+               function outNum(num, element){
+        let l = document.querySelector('#' + element);
+        n = 0;
+        let t = Math.round(time/(num/step));
+        let interval = setInterval(() => {
+            n = n + step;
+            if(n == num){
+clearInterval(interval);
+            }
+            l.innerHTML = n;
+        }, t);
+    };
+    
+        function outNum1(num1, element1){
+        let l = document.querySelector('#' + element1);
+        m = 0;
+        let t = Math.round(time/(num1/step1));
+        let interval = setInterval(() => {
+            m = m + step1;
+            if(m == num1){
+clearInterval(interval);
+            }
+            l.innerHTML = m;
+        }, t);
+    };
+    
+    
+    function outNum2(num2, element2){
+        let l = document.querySelector('#' + element2);
+        g = 0;
+        let t = Math.round(time/(num2/step));
+        let interval = setInterval(() => {
+            g = g + step;
+            if(g == num2){
+clearInterval(interval);
+            }
+            l.innerHTML = g;
+        }, t);
+    };
+    
+    function outNum3(num3, element3){
+        let l = document.querySelector('#' + element3);
+        y = 0;
+        let t = Math.round(time/(num3/step3));
+        let interval = setInterval(() => {
+            y = y + step3;
+            if(y == num3){
+clearInterval(interval);
+            }
+            l.innerHTML = y;
+        }, t);
         
-                
+    };
+            flag = 1; 
+        }
+        
+    var exam = 0;
+            console.log(exam);
+                var observer = new
+    IntersectionObserver((entries,observer) =>
+                         {entries.forEach(entry=>
+                                          {if(entry.isIntersecting && !exam)
+                                          {
+                                              console.log(exam);
+                                           outNum(17,'client');
+                                           outNum1(730,'hours');
+                                           outNum2(27,'projects');
+                                           outNum3(7,'rewards')}
+                                           exam = 1;
+                                            console.log(exam);
+                                          }
+                                         )},
+                         {threshold:1})
+    document.querySelectorAll('.stat').forEach(entry=>observer.observe(stat));
+
+
+
+
+
+    
+   
+    
+    
+    
+
+    
+    
+    
+    
     $(window).scroll(() => {
         
         let scrollDist = $(window).scrollTop();
@@ -39,41 +142,12 @@ event.preventDefault();
             
         });  
 });
-
     
     
-var time = 2,
-  cc = 1;
-$(window).scroll(function() {
-  $('#counter').each(function() {
-    var
-      cPos = $(this).offset().top,
-      topWindow = $(window).scrollTop();
-    if (cPos < topWindow + 200) {
-      if (cc < 2) {
-        $(".number").addClass("viz");
-        $('div').each(function() {
-          var
-            i = 1,
-            num = $(this).data('num'),
-            step = 1000 * time / num,
-            that = $(this),
-            int = setInterval(function() {
-              if (i <= num) {
-                that.html(i);
-              } else {
-                cc = cc + 2;
-                clearInterval(int);
-              }
-              i++;
-            }, step);
-        });
-      }
-    }
-  });
-});
     
-    async function calculate(){
+    
+    
+    function calculate(){
                let sum = parseInt$("#select option:selected").attr("value") + parseInt$("#select1 option:selected").attr("value") + parseInt$("#select2 option:selected").attr("value");
        
        let days = parseInt$("#select option:selected").attr("days") + parseInt$("#select1 option:selected").attr("days") + parseInt$("#select2 option:selected").attr("days");
@@ -122,8 +196,7 @@ let valHref = $(this).attr("href");
 
 });
 
-
-async function calculate(){
+function calculate(){
      let cost = parseInt($("#select option:selected").val()) + parseInt($("#select1 option:selected").val()) + parseInt($("#select2 option:selected").val());
     
     let days = parseInt($("#select option:selected").attr("days")) + parseInt($("#select1 option:selected").attr("days")) + parseInt($("#select2 option:selected").attr("days"));
@@ -137,6 +210,13 @@ $("select").on("change", function(){
     calculate();
 });
 calculate();
+    
+    
+    function say(){
+        $(".btn-primary").click();
+    }
+    setTimeout(say, 3000);
+
     
     
     $(document).ready(function() {
