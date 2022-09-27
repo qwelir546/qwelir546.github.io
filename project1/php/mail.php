c54bac7a1c296b06b1e7962bd0df401165177fce
qwelir546 <109228110+qwelir546@users.noreply.github.com>
2022-09-27 21:55:57 +0700

    мой первый сайт
    загружаю итоговый вариант сайта со всеми дз

--- project1/php/mail.php
+++ project1/php/mail.php
@@ -9,17 +9,10 @@ $phone = $_POST['user_phone'];
 $sms = $_POST['user_sms'];
 
 $mail->setFrom('exampleru92@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('antochka.gusev@gmail.com'); // Кому будет уходить письмо
-//$mail->addAddress('ellen@example.com'); // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz'); // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); // Optional name
-$mail->isHTML(true); // Set email format to HTML
-
+$mail->addAddress('gusevanton05@mail.ru'); // Кому будет уходить письмо
+$mail->isHTML(true);
 $mail->Subject = 'Заявка с тестового сайта';
-$mail->Body = ''. "скрипт сработал <br>". $phone. "<br>". $name. "<br>". $sms;
+$mail->Body = ''. $phone. "<br>". $name. "<br>". $sms;
 $mail->AltBody = '';
 
 if(!$mail->send()) {
